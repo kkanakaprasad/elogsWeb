e6cbfe9183c4417be71bc0431dff94231c8a8eb9
kkanakaprasad <109482694+kkanakaprasad@users.noreply.github.com>
2022-12-16 16:38:29 +0530

    organization list is completed

--- src/app/organization/organization-list/organization-list.component.ts
+++ src/app/organization/organization-list/organization-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpDataService } from 'src/app/shared/services/http-service/http-service.service';
+import { AddNewUserService } from 'src/app/user/add-new-user/add-new-user.service';
+import { OrganizationService } from '../organization.service';
 
 @Component({
   selector: 'app-organization-list',
@@ -6,10 +9,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./organization-list.component.scss']
 })
 export class OrganizationListComponent implements OnInit {
+  organizationList: any;
 
-  constructor() { }
+  constructor(private organizationService:OrganizationService,
+    private addNewUserService:AddNewUserService) { }
+
 
   ngOnInit(): void {
+    this.getAllOrganizationsSearchCriteria()
+  }
+
+  getAllOrganizationsSearchCriteria(){
+    const payload={
+      pageNumber: 0,
+      pageSize: 50,
+      sortField: "",
+      sortOrder: 0,
+      type: "",
+      organization: ""
+    }
+    this.organizationService.getOrganizationsSearchCriteria(payload).subscribe((res)=>{
+      this.organizationList=res.organizations[0].organizations.reverse();
+      console.log(this.organizationList)
+    })
+    
+  }
+
+  addOrganizationList(){
+    this.organizationService.openCreateOrganizatioPopup().afterClosed().subscribe((res)=>{
+      if(res){
+        this.getAllOrganizationsSearchCriteria()
+      }
+    })
+  }
+  addUser(){
+    this.addNewUserService.openAddUser()
   }
 
 }
