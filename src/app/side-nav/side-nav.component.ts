609ac0b8c897f72222386cbdac4ff31fdba7de57
J Prashanth Kumar <83943164+prashanthj32@users.noreply.github.com>
2022-12-16 14:35:16 +0530

    modefied side nav

--- src/app/side-nav/side-nav.component.ts
+++ src/app/side-nav/side-nav.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { RouteConstants } from '../shared/constants/routes.constants';
+import { Roles } from '../shared/enums/roles.enums';
+import { STORAGE_KEYS } from '../shared/enums/storage.enum';
+import { StorageService } from '../shared/services/storage-service/storage.service';
 import { UserDetails } from '../shared/services/user-details-service/user-details.interface';
 import { UserDetailsService } from '../shared/services/user-details-service/user-details.service';
 
@@ -14,16 +19,29 @@ export class SideNavComponent implements OnInit {
     email: '',
     roles: ['']
   }
-  constructor(private userDetailsService:UserDetailsService) { }
+  isSuperAdmin : boolean = false;
+
+  constructor(private userDetailsService:UserDetailsService, 
+    private storageService:StorageService,
+    private router : Router) { }
 
   ngOnInit(): void {
     this.getLogedInUserDeatils()
+    this.isSuperAdmin = this.storageService.getDataFromLocalStorage(STORAGE_KEYS.ROLE) === Roles.SuperAdmin ? true : false
   }
 
   getLogedInUserDeatils(){
     this.userDetailsService.getUserDetails().subscribe((res)=>{
       this.logedinUserDetails = res;
     })
+  }
+
+  navigateToOrganizationList(){
+    this.router.navigate([RouteConstants.ORAGANIZATION_LIST])
+  }
+
+  naviagteToDashboard(){
+    this.router.navigate([RouteConstants.DASHBOARD]);
   }
 
 }
