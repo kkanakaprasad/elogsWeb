ebf2a81e18943fa8668adb2bc51b772b47988f68
kkanakaprasad <109482694+kkanakaprasad@users.noreply.github.com>
2022-12-24 12:17:03 +0530

    create organization was working fine need to fix some bugs

---
+++ src/app/organization/remove-user-pop-up/remove-user-pop-up.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { RemoveUserPopUpComponent } from './remove-user-pop-up.component';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RemoveUserPopUpService {
+
+  constructor(private matDialog: MatDialog) { }
+  removeUserPopUp(selectedOrganizationId:string) {
+    return this.matDialog.open(RemoveUserPopUpComponent, { disableClose : true,
+      minWidth: '500px',data:selectedOrganizationId
+    })
+  }
+}
