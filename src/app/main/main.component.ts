2b3b2ca19cad11226cdfe51b77c40d72d24c27a2
Nagendra <nagkodama@gmail.com>
2023-01-09 18:38:40 +0530

    added filter organization in main component

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CompanySettingsService } from '../company-settings/company-settings.service';
+import { SearchPipe } from '../shared/pipes/search.pipe';
 
 @Component({
   selector: 'app-main',
@@ -6,10 +8,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
+  organizationsList = [] as any;
+  searchedOrganizationList : any = [];
 
-  constructor() { }
+  constructor(private companySettingsService: CompanySettingsService) { }
 
   ngOnInit(): void {
+    this.getAllOrganizations()
   }
+
+  getAllOrganizations() {
+    this.companySettingsService.getAllOrganizations().subscribe((res) => {
+      this.organizationsList = res.organizations
+      this.searchedOrganizationList = res.organizations
+      console.log(this.organizationsList)
+    })
+  }
+
+  // filterOrganization(event:any) {
+  //   const searchText = event.target.value;
+
+  //   this.organizationsList = this.organizationsList.map((org: any) => {
+  //     return new SearchPipe().transform( org, searchText, org.organization)
+  //     //return org.organization.toLowerCase().indexOf(searchText.toLowerCase()) > -1
+  //   });
+  // }
+  filterOrganization(event:any){
+    if(event.target.value){
+      const search = new SearchPipe();
+      this.searchedOrganizationList = search.transform(this.organizationsList,event.target.value,'organization');
+    }else{
+      this.organizationsList;
+    }
+    
+    }
+    
+
+  
 
 }
