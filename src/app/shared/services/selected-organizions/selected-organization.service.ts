a25f47e613bd929bec31c5cbc7c16809a70b16df
Nagendra <nagkodama@gmail.com>
2023-01-09 21:42:47 +0530

    added selected organization service

---
+++ src/app/shared/services/selected-organizions/selected-organization.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SelectedOrganizationService {
+
+  constructor() { }
+
+ 
+
+  private selectedOrganization : BehaviorSubject<any> = new BehaviorSubject({});
+
+  public getSelectedOrganization():Observable<any>{
+    return this.selectedOrganization
+  }
+
+  public setSelectedOrganization(selectedOrganization:any){
+    this.selectedOrganization.next(selectedOrganization);
+  }
+
+}
