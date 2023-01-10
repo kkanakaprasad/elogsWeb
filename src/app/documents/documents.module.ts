8777cc7d9b123c563e58c8b7d9ab2cb77723b3c9
Nagendra <nagkodama@gmail.com>
2023-01-10 18:46:59 +0530

    added component in declaration in module

--- src/app/documents/documents.module.ts
+++ src/app/documents/documents.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DocumentsRoutingModule } from './documents-routing.module';
+import { DocumentsComponent } from './documents.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [DocumentsComponent],
   imports: [
     CommonModule,
-    DocumentsRoutingModule
+    DocumentsRoutingModule,
+    SharedModule
   ]
 })
 export class DocumentsModule { }
