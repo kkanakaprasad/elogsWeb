4683db7fee650047ae1ae4c72b5804579a17602e
Venkatadri <villagetechvenkat@gmail.com>
2023-01-27 19:10:51 +0530

    Added DashBoard Component ToDO's Added Organisation Short Name In UpperCase And Max 5 Characters Changed Password hint message

--- src/app/user/user-list/user-Interface.ts
+++ src/app/user/user-list/user-Interface.ts
@@ -7,5 +7,26 @@ export interface UserSearchCriteria {
     isActive?: boolean,
     role: string,
     userId: string,
-    user:string
+    user:string,
+}
+
+export interface OverDueTaskSearchCriteria{
+    pageNumber:number,
+    pageSize:number,
+    sortField:string,
+    sortOrder:number,
+    isArchive:false,
+    dueDate:any,
+    title:string
+}
+
+export interface UpComingTaskSearchCriteria{
+    pageNumber:0,
+    pageSize:5,
+    sortField:string,
+    sortOrder:0,
+    isArchive:false,
+    status:["NEW","INPROGRESS"],
+    dueDate:any,
+    title:string
 }
