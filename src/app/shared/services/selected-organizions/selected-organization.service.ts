2fe72901eed9e3d1a19d3faab381a883e656e7f1
J Prashanth Kumar <83943164+prashanthj32@users.noreply.github.com>
2023-01-31 17:38:26 +0530

    added jwt decoder updated user details service added selected organization based acrtivities in activities list page

--- src/app/shared/services/selected-organizions/selected-organization.service.ts
+++ src/app/shared/services/selected-organizions/selected-organization.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
+import { Roles } from '../../enums/roles.enums';
+import { UserDetailsService } from '../user-details-service/user-details.service';
 
 
 @Injectable({
@@ -7,15 +9,25 @@ import { BehaviorSubject, Observable } from 'rxjs';
 })
 export class SelectedOrganizationService {
 
-  constructor() { }
-
-  private selectedOrganization : BehaviorSubject<any> = new BehaviorSubject([]);
-
-  public getSelectedOrganization():Observable<any>{
-    return this.selectedOrganization
+  constructor(
+    private userDetailsService: UserDetailsService,
+  ) {
+    this.userDetailsService.getUserDetails().subscribe((res: any) => {
+        if(res?.roles[0] !== Roles.SuperAdmin){
+          this.setSelectedOrganization(res.organization);
+        }else{
+          this.setSelectedOrganization([]);
+        }
+    })
   }
 
-  public setSelectedOrganization(selectedOrganization:any){
+  private selectedOrganization: BehaviorSubject<any> = new BehaviorSubject(null);
+
+  public getSelectedOrganization(): Observable<any> {
+    return this.selectedOrganization.asObservable();
+  }
+
+  public setSelectedOrganization(selectedOrganization: any) {
     this.selectedOrganization.next(selectedOrganization);
   }
 
