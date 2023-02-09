645c3814970d49a8ebacdd625a537767cd9ecd66
Venkatadri <villagetechvenkat@gmail.com>
2023-02-09 19:44:08 +0530

    Changed count To todoActivityCount

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -39,7 +39,7 @@ export class DashboardComponent implements OnInit {
   overDueActivities: any = [];
   upComingActivities: any = [];
   isSuperAdmin!: boolean;
-  count: any;
+  todoActivityCount: any;
 
 
 
@@ -214,7 +214,7 @@ export class DashboardComponent implements OnInit {
     }
     this.activityService.getActivitiesSearchCriteria(payload).subscribe((res) => {
       this.upComingActivities = res?.data[0].activities;
-      this.count = res?.data[0].count[0].count;
+      this.todoActivityCount = res?.data[0].count[0].count;
     })
   }
 
